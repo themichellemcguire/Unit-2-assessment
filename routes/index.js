d5d76888dec44d5c5d2c588a707e0d61610a2f22
owner <themichellemcguire@gmail.com>
2019-03-03 15:24:09 -0600

    changed name and id in index

--- routes/index.js
+++ routes/index.js
@@ -5,45 +5,40 @@ const todos = require('../data/todos')
 /* GET home page. */
 
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Unit 2 Assessment', todos: todos });
+  res.render('index', { title: 'Unit 2 Assessment', todos });
 });
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
 // This is the index route to show all the todos
-router.get('/', function(req, res){
-    // We need to query the database to get all of the todos
-    todos.find({}, function(error, todos){
-        if(error){
-            console.log(error)
-        } else {
-            console.log(todos, 'This is all the todos')
-            res.render('index', {todos})
-        }
-    })
-})
+
+// router.get('/', function(req, res){
+//     // We need to query the database to get all of the todos
+//     todos.find({}, function(error, todos){
+//         if(error){
+//             console.log(error)
+//         } else {
+//             console.log(todos, 'This is all the todos')
+//             res.render('index', {title: 'Unit 2 Assessment',todos})
+//         }
+//     })
+// })
 
 // This is the route to post a new todo to the Database
 
 router.post('/', addItem);
 
-function addItem(req, res, next) {
-  todos.push({item: req.body.todo, done: false})
+function addItem(req, res) {
+  todos.push({item: req.body.todos, done: false})
   res.redirect('/');
 }
 
 // Delete todos by it's id
-router.delete('/:id', function(req, res){
-    console.log(req.params.id, 'Id in the delete route')
-    todos.findByIdAndRemove(req.params.id, function(error, deletedTodos){
-        if(error){
-            console.log(error)
-        } else {
-            console.log(deletedtodos, 'This todos was deleted')
-            res.redirect('/')
-        }
-    })
-})
+// router.delete('/:id', remove);
 
-module.exports = router
+// router.delete('/:id', function remove(req, res) {
+//         todos.findOneAndRemove({'_id': req.params.id}, function(err, todos) {res.redirect('/');
+// });
+
+module.exports = router;
